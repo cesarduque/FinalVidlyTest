b75662a818f85c8058755e786b4f38aed404ceea
Cesar Anibal Duque Gomez <cesar.duque@yuxiglobal.com>
2018-09-05 17:30:28 -0500

    Fist Change

---
+++ FinalProjectVidly/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using FinalProjectVidly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProjectVidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+    }
+}
